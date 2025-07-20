f226512ca9f19da76a491d85f8dcfc5d2947c2f4
Shubham Kumar <Shubhamshk07@gmail.com>
2025-07-20 11:17:45 +0530

    done with feedback page and  final submission

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
-    { title: 'Resumind | Auth' },
+    { title: 'ResumeBuddy | Auth' },
     { name: 'description', content: 'Log into your account' },
 ])
 
@@ -18,7 +18,7 @@ const Auth = () => {
     }, [auth.isAuthenticated, next])
 
     return (
-        <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
                 <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
                     <div className="flex flex-col items-center gap-2 text-center">
